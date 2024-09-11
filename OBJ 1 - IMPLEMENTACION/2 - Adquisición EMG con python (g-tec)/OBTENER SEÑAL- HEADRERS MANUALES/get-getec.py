13179482cdb09b834630739c06f054019ae1a162
Willandru <willandru@hotmail.com>
2024-09-11 07:43:00 -0500

    SEÑAL EMG: si se adquiere con headers MANUALES, se obtuvo el scope

--- OBJ 1 - IMPLEMENTACION/2 - Adquisición EMG con python (g-tec)/OBTENER SEÑAL- HEADRERS MANUALES/get-getec.py
+++ OBJ 1 - IMPLEMENTACION/2 - Adquisición EMG con python (g-tec)/OBTENER SEÑAL- HEADRERS MANUALES/get-getec.py
@@ -12,4 +12,27 @@ gNEEDaccessHeaders = [
 result = pygds.Initialize(gds_headers=gNEEDaccessHeaders)
 
 print(result)
+d = pygds.GDS()
 
+#Selecciona en 'Sampling Rate' y 'Number of Scans'
+f_s_2 = sorted(d.GetSupportedSamplingRates()[0].items())[6]
+d.SamplingRate, d.NumberOfScans = f_s_2
+# Activar un canal específico
+for i, channel in enumerate(d.Channels):
+    if i == 0:
+        channel.Acquire = 1
+        channel.BandpassFilterIndex = 123
+        channel.NotchFilterIndex = 9
+    else:
+        channel.Acquire = 0
+# Aumentar la escala del algún canal.
+current_scaling = d.GetScaling()
+current_scaling[0].ScalingFactor = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+d.SetScaling(current_scaling)
+
+print(d.GetScaling())
+#Fija la configuración anterior en el dispositivo
+d.SetConfiguration()
+# Muestra un Osciloscopio en vivo de la señal
+scope = pygds.Scope(1/d.SamplingRate) 
+d.GetData(d.SamplingRate,scope)
